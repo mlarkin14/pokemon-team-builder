08ed7991ea4ddbab4d965fda258a2a95a4f85c90
Matt Larkin <mlarkin14@gmail.com>
2022-06-12 16:06:48 -0400

    passportjs functionality

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
+var authRouter = require('./controllers/auth');
+var passport = require('passport');
+var session = require('express-session');
 
+var SQLiteStore = require('connect-sqlite3')(session);
 router.use('/', homeRoutes);
+app.use('/', authRouter);
 
 module.exports = router;
