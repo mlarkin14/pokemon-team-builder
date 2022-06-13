d6fea2621f6374a5dac4467a94e7ef3e882cd885
mrsdno <christine@dno.dev>
2022-06-13 18:37:29 -0400

    added more information to gallery page, simply css styling

--- public/javascript/pokemon.js
+++ public/javascript/pokemon.js
@@ -23,15 +23,38 @@ function fetchPokemonData(pokemon) {
 var pokemonHolder = document.querySelector(".pokemon-holder");
 
 function appendPokemon(pokemon) {
-    // create pokemon name element
-    var pokemonNameElement = document.createElement('h1');
+  // create pokemon element
+  const pokemonElement = document.createElement("div");
 
-    // fill with appropiate data
-    pokemonNameElement.textContent = pokemon.species.name;
+  // assign information to variables for use
+  const pokemonName = pokemon.species.name;
+    const pokemonImg = pokemon.sprites.front_default;
+    // use loop for pokemon type in case there is more than one
+    let pokemonType = ""
+    for (let i = 0; i < pokemon.types.length; i++) {
+        pokemonType = pokemonType.concat(pokemon.types[i].type.name + " ");
+    }
 
-    //append to page
-    pokemonHolder.appendChild(pokemonNameElement);
+  // put pokemon information inside the div
+  // name
+  const pokemonNameEl = document.createElement("h1");
+  pokemonNameEl.textContent = pokemonName;
+  pokemonElement.appendChild(pokemonNameEl);
+  pokemonElement.setAttribute("data-name", pokemonName);
 
+  // picture
+  const pokemonPictureEl = document.createElement("img");
+  pokemonPictureEl.src = pokemonImg;
+  pokemonElement.appendChild(pokemonPictureEl);
+  pokemonElement.setAttribute("data-img", pokemonImg);
+
+  const pokemonTypeEl = document.createElement("p");
+  pokemonTypeEl.textContent = pokemonType;
+  pokemonElement.appendChild(pokemonTypeEl);
+  pokemonElement.setAttribute("data-type", pokemonType);
+
+  //append to page
+  pokemonHolder.appendChild(pokemonElement);
 }
 
 fetchPokemon();
