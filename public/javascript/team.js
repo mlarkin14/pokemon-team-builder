e03e1738914780c6218781339ed4debd15c3f92a
ckim14 <chikakim@gmail.com>
2022-06-19 22:40:04 -0400

    created solution to reload page to display the pokemon  after pokemon is added

--- public/javascript/team.js
+++ public/javascript/team.js
@@ -1,25 +1,23 @@
 function viewGallery(event) {
-    event.preventDefault();
-    console.log('you clicked the button');
-    document.location.replace('/pokemon');
+  event.preventDefault();
+  console.log("you clicked the button");
+  document.location.replace("/pokemon");
 }
 
 // add listener to pokemon buttons in loop
-pokemonBtns = document.getElementsByClassName('add-pokemon');
+pokemonBtns = document.getElementsByClassName("add-pokemon");
 
 for (var i = 0; i < pokemonBtns.length; i++) {
-    pokemonBtns[i].addEventListener('click', viewGallery);
+  pokemonBtns[i].addEventListener("click", viewGallery);
 }
-
 
 const deleteBtn = document.querySelector("#delete-pokemon");
 
 async function deletePokemon(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-
-    //get id from delete btn
-    const id = deleteBtn.dataset.id;
+  //get id from delete btn
+  const id = deleteBtn.dataset.id;
   if (confirm("Are you sure you want to delete this pokemon?") == true) {
     const response = await fetch(`/api/pokemon/${id}`, {
       method: "DELETE",
@@ -36,9 +34,14 @@ async function deletePokemon(event) {
   }
 }
 
-
 deleteBtns = document.getElementsByClassName("delete-pokemon");
 for (var i = 0; i < deleteBtns.length; i++) {
-    deleteBtns[i].addEventListener('click', deletePokemon);
+  deleteBtns[i].addEventListener("click", deletePokemon);
 }
 
+window.onload = function () {
+  if (!window.location.hash) {
+    window.location = window.location + "#loaded";
+    window.location.reload();
+  }
+};
